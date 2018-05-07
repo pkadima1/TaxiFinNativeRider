4742126c85f328cc3875eafc23675d8268caa448
Patrick Tshims <pkadima1@gmail.com>
2018-05-07 10:46:44 +0300

    Geo Fire add to rider. Tut6

---
+++ app/src/main/java/com/taxifin/tshims/taxifinrider/Helper/CustomInfoWindow.java
@@ -0,0 +1,37 @@
+package com.taxifin.tshims.taxifinrider.Helper;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+import com.taxifin.tshims.taxifinrider.R;
+
+public class CustomInfoWindow implements GoogleMap.InfoWindowAdapter{
+
+    View myView;
+
+    public CustomInfoWindow(Context context) {
+        myView = LayoutInflater.from(context)
+                .inflate(R.layout.custom_rider_info_window, null);
+
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        TextView txtPickupTittle = ((TextView)myView.findViewById(R.id.txtPickupInfo));
+        txtPickupTittle.setText(marker.getTitle());
+
+        TextView txtPickupSnippet = ((TextView)myView.findViewById(R.id.txtPickupSnippet));
+        txtPickupSnippet.setText(marker.getSnippet());
+
+        return myView;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+}
